c1c6d796f14d1b77c715f9d635866b2f72e53422
3185281991 <82033448+3185281991@users.noreply.github.com>
2022-04-10 17:37:07 +0800

    Delete src directory

--- src/network/category.js
+++
@@ -1,6 +0,0 @@
-import { request } from "./request";
-export function getCategory() {
-  return request({
-    url: "/category",
-  });
-}
--- src/store/mutations-type.js
+++
@@ -1,2 +0,0 @@
-export const ADD_COUNTER = "add_counter";
-export const ADD_TO_CART = "add_to_cart";
